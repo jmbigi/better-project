c061bac1a5d23e3b7ca76f1d5c209e4902594716
jmbigi <jmbigi@users.noreply.github.com>
2026-09-24 11:21:22 -0300

    run test y simple test

---
+++ run_test.py
@@ -0,0 +1,8 @@
+import unittest
+import sys
+
+loader = unittest.TestLoader()
+suite = loader.loadTestsFromName('tests.test_ecosistema.TestJevReview')
+runner = unittest.TextTestRunner(verbosity=2, stream=sys.stdout)
+result = runner.run(suite)
+sys.exit(0 if result.wasSuccessful() else 1)
